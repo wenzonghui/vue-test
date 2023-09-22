5175ef6d2bfc40d0253a9c7e5ea4ee1dfb17c8f5
wenzonghui <wenzonghui@gmail.com>
2023-09-22 20:34:27 +0800

    slot 插槽案例3

---
+++ slot-03/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <SlotA api-url="url" :per-page="10">
+    <template #item="{body, username, likes}">
+      <div class="item">
+        <p>{{ body }}</p>
+        <p class="meta">by {{ username }} | {{ likes }} likes</p>
+      </div>
+    </template>
+  </SlotA>
+</template>
+
+<script setup lang="ts">
+import SlotA from './views/SlotA.vue';
+</script>
+
+
+<style scoped>
+.meta {
+  font-size: 0.8em;
+  color: #42b883;
+}
+</style>
---
+++ slot-03/src/views/SlotA.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-if="!items.length">
+      Loading...
+    </li>
+
+    <li v-for="item in items">
+      <slot name="item" v-bind="item"/>
+    </li>
+  </ul>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(['api-url', 'per-page'])
+
+const items = ref([])
+
+setTimeout(() => {
+  items.value = [
+    {body: 'Scoped Slot Guide', username: 'Even You', likes: 20},
+    {body: 'Vue Tutorial', username: 'Natalia Tepluhina', likes: 10}
+  ]
+}, 1000)
+</script>
+
