b598bb0973c32f78385ebf435b71cec51500bb9b
wenzonghui <wenzonghui@gmail.com>
2023-09-22 19:50:06 +0800

    使用 name 具名插槽

---
+++ slot-02/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <SlotA>
+    
+    <template #header>
+      <p>header</p>
+    </template>
+
+    <template #default>
+      <p>main</p>
+    </template>
+
+    <template #footer>
+      <p>footer</p>
+    </template>
+
+  </SlotA>
+</template>
+
+
+<script setup lang="ts">
+import SlotA from './views/SlotA.vue';
+</script>
---
+++ slot-02/src/views/SlotA.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <header>
+            <slot name="header"></slot>
+        </header>
+
+        <main>
+            <slot name="default"></slot>
+        </main>
+
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<style>
+    header {
+        background-color: blue;
+    }
+
+    main {
+        background-color: burlywood;
+    }
+
+    footer {
+        font-size: 0.8em;
+        background-color: chartreuse;
+    }
+</style>
