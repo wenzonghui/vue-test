3788103bedbecdd4f3e96a3a6758d264a32a984d
wenzonghui <wenzonghui@gmail.com>
2023-09-23 20:03:54 +0800

    使用slot 实现鼠标坐标追踪

---
+++ slot-04/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <SlotA v-slot="x, y">
+    Mouse is at: {{ x }}, {{ y }}
+  </SlotA>
+</template>
+
+
+<script setup lang="ts">
+import SlotA from './views/SlotA.vue';
+</script>
---
+++ slot-04/src/views/SlotA.vue
@@ -0,0 +1,18 @@
+<template>
+  <slot :x="x" :y="y"/>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const x = ref(0)
+const y = ref(0)
+
+const update = e => {
+  x.value = e.pageX
+  y.value = e.pageY
+}
+
+onMounted(() => window.addEventListener('mousemove', update))
+onUnmounted(() => window.addEventListener('mousemove', update))
+</script>
